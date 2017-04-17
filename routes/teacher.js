408b747cf1fc05bd05f25d85b7f594170c6bbf01
Chanel Mepschen <chanelmepschen@Chanels-MacBook-Pro.local>
2017-04-17 15:45:24 +0200

    res.send

--- routes/teacher.js
+++ routes/teacher.js
@@ -5,21 +5,27 @@ var request = require('request');
 router.get('/', function(req, res){
   var studentID = [
     '8EA6',
-    'CB1B'
+    'CB1B',
+    '180F'
   ];
 
   studentID.forEach(function(student){
     request(('https://oege.ie.hva.nl/~palr001/icu/api.php?t=sdc&d=180F&td='+ student + '&c=00ff00&m=Iedereensnapthet!'), function(error, response, data){
       if(error){
         console.log(error);
+        console.log('level 1: NIET');
       } else {
+        console.log('level 1: WEL');
         request('https://oege.ie.hva.nl/~palr001/icu/api.php?t=sqi&d=180F', function (error, response, message){
           if(error){
             console.log(error);
+            console.log('level 2: NIET');
           }
-      });
+        });
+      }
     });
   });
+
+  res.send('ok');
 });
-//00ff00
 module.exports = router;
