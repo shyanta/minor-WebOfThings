9e61e07f7c99cdd16b0ddb929543b403b764c16e
Elton Goncalves <eltongonc@gmail.com>
2017-04-17 21:34:36 +0200

    Removed unused stuff. Added a eslinter to check before git commit

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
-
-
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var exphbs  = require('express-handlebars');
+var express = require('express'),
+    path = require('path'),
+    // favicon = require('serve-favicon'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    exphbs  = require('express-handlebars');
 
 var routes = require('./routes/index');
 
@@ -19,17 +16,16 @@ app.locals.ENV_DEVELOPMENT = env == 'development';
 // view engine setup
 
 app.engine('handlebars', exphbs({
-  defaultLayout: 'main',
-  partialsDir: ['views/partials/']
+    defaultLayout: 'main',
+    partialsDir: ['views/partials/']
 }));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'handlebars');
 
 // app.use(favicon(__dirname + '/public/img/favicon.ico'));
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-  extended: true
+    extended: true
 }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
@@ -49,7 +45,7 @@ app.use(function(req, res, next) {
 // will print stacktrace
 
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
+    app.use(function(err, req, res) {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
@@ -61,7 +57,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,34 @@
 var express = require('express');
 var request = require('request');
-var router = express.Router();
 
+var router = express.Router();
 var counter = 0;
 
 router.get('/', function(req, res){
     counter++;
-  request('https://oege.ie.hva.nl/~palr001/icu/api.php?t=sdc&d=180F&td=8EA6&c=ff6200&m=Hoi', function (error, response, data){
-    if(error){
-      console.log(error);
-    } else {
-      console.log(data);
-      request('https://oege.ie.hva.nl/~palr001/icu/api.php?t=sqi&d=180F', function (error, response, message){
+    request('https://oege.ie.hva.nl/~palr001/icu/api.php?t=sdc&d=180F&td=8EA6&c=ff6200&m=Hoi', function (error, response, data){
         if(error){
-          console.log(error);
+            console.log(error);
+        } else {
+            console.log(data);
+            request('https://oege.ie.hva.nl/~palr001/icu/api.php?t=sqi&d=180F', function (error, response, message){
+                if(error){
+                    console.log(error);
+                }
+                console.log(message);
+                res.redirect('/test');
+            });
         }
-        console.log(message);
-        res.redirect("/test");
-      });
-    }
-  });
+    });
 });
 
 router.get('/test', function(req, res){
-    res.render("test",{title: counter});
+    res.render('test',{title: counter});
 });
 
 router.get('/reset', function(req, res){
     counter = 0;
-    res.render("test",{title: counter});
+    res.render('test',{title: counter});
 });
 
 module.exports = router;
